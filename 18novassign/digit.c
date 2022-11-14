623a2b9204f8bda7a73e5b95fea5219a591e4a79
anonmityslayer <anonmityslayer@tutanota.com>
2022-11-14 20:19:39 +0530

    14 nov assignment added

---
+++ 18novassign/digit.c
@@ -0,0 +1,36 @@
+/* Write a program to find the sum of even and odd digits of a number separately.
+
+Suppose the number is 12346
+
+The output will be:
+
+Even Digit Sum: 2+4+6=12
+Odd Digit Sum: 1+3=4 */
+#include <stdio.h>
+
+int main(void)
+{
+    int n,odd_sum,even_sum,r;
+    odd_sum = 0;
+    even_sum = 0;
+    printf("enter number \n");
+    scanf("%d",&n);
+
+    while(n!=0)
+    {
+        r = n%10;
+        if(r%2 == 0)
+        {
+            even_sum = even_sum+r;
+        }
+        else
+        {
+            odd_sum = odd_sum + r;
+        }
+        n = n/10;
+    }
+
+    printf("sum of digits(even by value) = %d \n",even_sum);
+    printf("sum of digits(odd by value) = %d \n",odd_sum);
+return 0;
+}
